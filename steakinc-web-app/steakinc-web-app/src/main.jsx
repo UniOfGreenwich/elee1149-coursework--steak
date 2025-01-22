9eab0c653610d78bc54e91c466ddde1ae24201e7
JBUCHA46 <jack.buchanan789@gmail.com>
2025-01-22 16:06:21 +0000

    All styling changes made and register screen added

--- steakinc-web-app/steakinc-web-app/src/main.jsx
+++ steakinc-web-app/steakinc-web-app/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import HomeScreen from './HomeScreen/HomeScreen.jsx';
 import LoginScreen from './LoginScreen/LoginScreen.jsx';
+import RegisterScreen from './RegisterScreen/RegisterScreen.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,7 +12,8 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
       </Routes>
     </Router>
-  </StrictMode>,
+  </StrictMode>
 );
