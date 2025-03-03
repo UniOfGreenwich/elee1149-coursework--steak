53056221df88496c4213632248cf1f640d4db278
hc5167i <hc5167i@gre.ac.uk>
2025-03-03 10:17:03 +0000

    Kick off styling for Gareth
    Kick off styling for Gareth

--- steakinc-web-app/steakinc-web-app/src/BudgetScreen/BudgetScreen.jsx
+++ steakinc-web-app/steakinc-web-app/src/BudgetScreen/BudgetScreen.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import NavSideBar from '../JarsScreen/NavSideBar'; // Import the NavSideBar component
 import './BudgetScreen.css';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -21,6 +22,7 @@ function BudgetScreen() {
     const [expenseCategory, setExpenseCategory] = useState('');
     const [expenseAmount, setExpenseAmount] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [categories, setCategories] = useState(['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Transportation']);
     const location = useLocation();
     const userId = location.state?.userId;
 
@@ -195,131 +197,151 @@ function BudgetScreen() {
     };
 
     return (
-        <div className="budget-container">
-            <h1>Budgeting</h1>
-            <h2>Total Income: £{totalIncome.toFixed(2)}</h2>
-            <h2>Remaining Balance: £{remainingBalance.toFixed(2)}</h2>
-            
-            <div className="budget-columns">
-                <div className="income-column">
-                    <h3>Incomes</h3>
-                    <button onClick={() => setShowIncomeModal(true)}>+ Add Income</button>
-                    <ul>
-                        {incomes.map(income => (
-                            <li key={income.income_id}>
-                                {income.name}: £{income.amount}
-                                <button onClick={() => openEditModal(income, 'Income')}>Edit</button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                
-                <div className="expense-column">
-                    <h3>Expenses</h3>
-                    <button onClick={() => setShowCreateExpenseModal(true)}>+ Add Expense</button>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Category</th>
-                                <th>Amount</th>
-                                <th>% of Total</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {expenses.map(expense => (
-                                <tr key={expense.budget_id}>
-                                    <td>{expense.expense}</td>
-                                    <td>{expense.category}</td>
-                                    <td>£{expense.amount}</td>
-                                    <td>{((expense.amount / totalIncome) * 100).toFixed(2)}%</td>
-                                    <td>
-                                        <button onClick={() => openEditModal(expense, 'Expense')}>Edit</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                    <div style={{ width: '50%', height: '400px', margin: '0 auto' }}>
+        <div>
+            <NavSideBar />
+            <div className="budget-main-container"> {/* Renamed this line */}
+                <div className="budget-container">
+                    <div className="budget-header">
+                        <h1>Budgeting</h1>
+                        <h2>Total Income: £{totalIncome.toFixed(2)}</h2>
+                        <h2>Remaining Balance: £{remainingBalance.toFixed(2)}</h2>
+                    </div>
+                    
+                    <div className="pie-chart-container">
                         <Pie data={expensePieData} />
                     </div>
-                </div>
-            </div>
 
-            {showIncomeModal && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2>Add Income</h2>
-                        <form onSubmit={handleAddIncome}>
-                            <label>Income Name: <input type="text" value={incomeName} onChange={(e) => setIncomeName(e.target.value)} required /></label>
-                            <label>Amount: <input type="number" value={incomeAmount} onChange={(e) => setIncomeAmount(e.target.value)} required /></label>
-                            <button type="submit">Add</button>
-                            <button type="button" onClick={() => setShowIncomeModal(false)}>Cancel</button>
-                        </form>
+                    <div className="budget-columns">
+                        <div className="income-column">
+                            <h3>Incomes</h3>
+                            <button onClick={() => setShowIncomeModal(true)}>+ Add Income</button>
+                            <ul>
+                                {incomes.map(income => (
+                                    <li key={income.income_id}>
+                                        {income.name}: £{income.amount}
+                                        <button onClick={() => openEditModal(income, 'Income')}>Edit</button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        
+                        <div className="expense-column">
+                            <h3>Expenses</h3>
+                            <button onClick={() => setShowCreateExpenseModal(true)}>+ Add Expense</button>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Category</th>
+                                        <th>Amount</th>
+                                        <th>% of Total</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {expenses.map(expense => (
+                                        <tr key={expense.budget_id}>
+                                            <td>{expense.expense}</td>
+                                            <td>{expense.category}</td>
+                                            <td>£{expense.amount}</td>
+                                            <td>{((expense.amount / totalIncome) * 100).toFixed(2)}%</td>
+                                            <td>
+                                                <button onClick={() => openEditModal(expense, 'Expense')}>Edit</button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </div>
-            )}
 
-            {showCreateExpenseModal && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2>Add Expense</h2>
-                        {errorMessage && <p className="error-message">{errorMessage}</p>}
-                        <form onSubmit={handleAddExpense}>
-                            <label>Expense Name: <input type="text" value={expenseName} onChange={(e) => setExpenseName(e.target.value)} required /></label>
-                            <label>Category: <input type="text" value={expenseCategory} onChange={(e) => setExpenseCategory(e.target.value)} required /></label>
-                            <label>Amount: <input type="number" value={expenseAmount} onChange={(e) => setExpenseAmount(e.target.value)} required /></label>
-                            <button type="submit">Add</button>
-                            <button type="button" onClick={() => setShowCreateExpenseModal(false)}>Cancel</button>
-                        </form>
-                    </div>
-                </div>
-            )}
+                    {showIncomeModal && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <h2>Add Income</h2>
+                                <form onSubmit={handleAddIncome}>
+                                    <label>Income Name: <input type="text" value={incomeName} onChange={(e) => setIncomeName(e.target.value)} required /></label>
+                                    <label>Amount: <input type="number" value={incomeAmount} onChange={(e) => setIncomeAmount(e.target.value)} required /></label>
+                                    <button type="submit">Add</button>
+                                    <button type="button" onClick={() => setShowIncomeModal(false)}>Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    )}
 
-            {showEditModal && selectedEntry && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2>Edit {selectedEntry.type}</h2>
-                        {errorMessage && <p className="error-message">{errorMessage}</p>}
-                        <form onSubmit={handleEditEntry}>
-                            <label>
-                                Name:
-                                <input type="text" value={selectedEntry.type === 'Income' ? selectedEntry.name : selectedEntry.expense} onChange={(e) => setSelectedEntry({ ...selectedEntry, name: e.target.value, expense: e.target.value })} required />
-                            </label>
-                            <br />
-                            {selectedEntry.type === 'Expense' && (
-                                <>
+                    {showCreateExpenseModal && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <h2>Add Expense</h2>
+                                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                                <form onSubmit={handleAddExpense}>
+                                    <label>Expense Name: <input type="text" value={expenseName} onChange={(e) => setExpenseName(e.target.value)} required /></label>
+                                    <label>Category: 
+                                        <select value={expenseCategory} onChange={(e) => setExpenseCategory(e.target.value)} required>
+                                            <option value="" disabled>Select category</option>
+                                            {categories.map((category, index) => (
+                                                <option key={index} value={category}>{category}</option>
+                                            ))}
+                                        </select>
+                                    </label>
+                                    <label>Amount: <input type="number" value={expenseAmount} onChange={(e) => setExpenseAmount(e.target.value)} required /></label>
+                                    <button type="submit">Add</button>
+                                    <button type="button" onClick={() => setShowCreateExpenseModal(false)}>Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    )}
+
+                    {showEditModal && selectedEntry && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <h2>Edit {selectedEntry.type}</h2>
+                                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                                <form onSubmit={handleEditEntry}>
                                     <label>
-                                        Category:
-                                        <input type="text" value={selectedEntry.category} onChange={(e) => setSelectedEntry({ ...selectedEntry, category: e.target.value })} required />
+                                        Name:
+                                        <input type="text" value={selectedEntry.type === 'Income' ? selectedEntry.name : selectedEntry.expense} onChange={(e) => setSelectedEntry({ ...selectedEntry, name: e.target.value, expense: e.target.value })} required />
                                     </label>
                                     <br />
-                                </>
-                            )}
-                            <label>
-                                Amount:
-                                <input type="number" value={selectedEntry.amount} onChange={(e) => setSelectedEntry({ ...selectedEntry, amount: e.target.value })} required />
-                            </label>
-                            <br />
-                            <button type="submit">Save Changes</button>
-                            <button type="button" onClick={() => setShowDeleteConfirmation(true)}>Delete {selectedEntry.type}</button>
-                            <button type="button" onClick={() => setShowEditModal(false)}>Cancel</button>
-                        </form>
-                    </div>
-                </div>
-            )}
+                                    {selectedEntry.type === 'Expense' && (
+                                        <>
+                                            <label>
+                                                Category:
+                                                <select value={selectedEntry.category} onChange={(e) => setSelectedEntry({ ...selectedEntry, category: e.target.value })} required>
+                                                    <option value="" disabled>Select category</option>
+                                                    {categories.map((category, index) => (
+                                                        <option key={index} value={category}>{category}</option>
+                                                    ))}
+                                                </select>
+                                            </label>
+                                            <br />
+                                        </>
+                                    )}
+                                    <label>
+                                        Amount:
+                                        <input type="number" value={selectedEntry.amount} onChange={(e) => setSelectedEntry({ ...selectedEntry, amount: e.target.value })} required />
+                                    </label>
+                                    <br />
+                                    <button type="submit">Save Changes</button>
+                                    <button type="button" onClick={() => setShowDeleteConfirmation(true)}>Delete {selectedEntry.type}</button>
+                                    <button type="button" onClick={() => setShowEditModal(false)}>Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    )}
 
-            {showDeleteConfirmation && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2>Delete {selectedEntry.type}</h2>
-                        <p>Are you sure you want to delete this {selectedEntry.type.toLowerCase()}?</p>
-                        <button onClick={handleDeleteEntry}>Delete</button>
-                        <button onClick={() => setShowDeleteConfirmation(false)}>Cancel</button>
-                    </div>
+                    {showDeleteConfirmation && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <h2>Delete {selectedEntry.type}</h2>
+                                <p>Are you sure you want to delete this {selectedEntry.type.toLowerCase()}?</p>
+                                <button onClick={handleDeleteEntry}>Delete</button>
+                                <button onClick={() => setShowDeleteConfirmation(false)}>Cancel</button>
+                            </div>
+                        </div>
+                    )}
                 </div>
-            )}
+            </div>
         </div>
     );
 }
