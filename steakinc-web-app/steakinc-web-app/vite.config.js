4fd795aff3d20e41ed5ad0d6405d4b24f446e0c0
hc5167i <hc5167i@gre.ac.uk>
2025-03-25 09:44:42 +0000

    Create vite.config.js

---
+++ steakinc-web-app/steakinc-web-app/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/elee1149-coursework--steak/', // 🔥 Change this to match your repo name
+});
